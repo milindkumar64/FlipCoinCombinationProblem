143d707cbaa5d15e765ee898de36b9072ce56006
milindkumar64 <milindgupta@gmail.com>
2023-03-06 19:39:38 +0530

    Add: Store the doublet combination and the percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,36 +1,60 @@
-isHead=1;
-isTail=2;
-headCount=0;
-tailCount=0;
-for ((i=1; i<=5; i++))
+isHeadHead=1;
+isHeadTail=2;
+isTailHead=3;
+isTailTail=4;
+headHeadCount=0;
+headTailCount=0;
+tailHeadCount=0;
+tailTailCount=0
+
+for ((i=1; i<=10; i++))
 do
-coinFace=$((1+RANDOM%2));
+coinFace=$((1+RANDOM%4));
 
 declare -A singlet
-#singlet[1]="H"
 
 case $coinFace in
-	          $isHead)
- 	          headCount=$(($headCount+1))
-                  singlet[$i]="H"
-                  echo "H " $i
+	          $isHeadHead)
+ 	          headHeadCount=$(($headHeadCount+1))
+                  singlet[$i]="HH"
+                  echo "HH " $i
 	          ;;
-	          $isTail)
-	          tailCount=$(($tailCount+1))
-                  singlet[$i]="T"
-                  echo "T " $i
+	          $isHeadTail)
+	          headTailCount=$(($headTailCount+1))
+                  singlet[$i]="HT"
+                  echo "HT " $i
 	          ;;
+                  $isTailHead)
+                  tailHeadCount=$(($tailHeadCount+1))
+                  singlet[$i]="TT"
+                  echo "TH " $i
+                  ;;
+                  $isTailTail)
+                  tailTailCount=$(($tailTailCount+1))
+                  singlet[$i]="TT"
+                  echo "TT " $i
+                  ;;
+
 esac
 
 done
-	echo "Head won $headCount times"
+	echo "HH won $headHeadCount times"
+        echo "TH won $tailHeadCount times"
+        echo "HT won $headTailCount times"
+        echo "TT won $tailTailCount times"
 
-
-	echo "Tail won $tailCount times"
         echo "All Singlet are :"${singlet[*]}
+        echo "All Singlet orders are :"${!singlet[*]}
 
-headPercentage=$(($headCount*100/5))
-echo "headPercentage is :" $headPercentage
+hHPercentage=$(($headHeadCount*100/10))
+echo "hHPercentage is :"$hHPercentage
 
-tailPercentage=$(($tailCount*100/5))
-echo "tailPercemtage is :" $tailPercentage
+hTPercentage=$(($headTailCount*100/10))
+echo "hTPercentage is :"$hTPercentage
+
+tHPercentage=$(($tailHeadCount*100/10))
+echo "tHPercentage is :"$tHPercentage
+
+tTPercentage=$(($tailTailCount*100/10))
+echo "tTPercentage is :"$tTPercentage
+
