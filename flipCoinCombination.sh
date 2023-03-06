a5a5e851d1eb5533b320d62e516f488dce9a510e
milindkumar64 <milindgupta@gmail.com>
2023-03-06 17:09:37 +0530

    Add: store the Singlet Combination in a Dictionary and determine the percentage of the Singlet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,11 +1,36 @@
 isHead=1;
 isTail=2;
+headCount=0;
+tailCount=0;
+for ((i=1; i<=5; i++))
+do
+coinFace=$((1+RANDOM%2));
 
-coinFlip=$((1+RANDOM%2));
+declare -A singlet
+#singlet[1]="H"
 
-if [ $isHead -eq $coinFlip ]
-then
-   echo "Head"
-else
-   echo "Tail"
-fi
+case $coinFace in
+	          $isHead)
+ 	          headCount=$(($headCount+1))
+                  singlet[$i]="H"
+                  echo "H " $i
+	          ;;
+	          $isTail)
+	          tailCount=$(($tailCount+1))
+                  singlet[$i]="T"
+                  echo "T " $i
+	          ;;
+esac
+
+done
+	echo "Head won $headCount times"
+
+
+	echo "Tail won $tailCount times"
+        echo "All Singlet are :"${singlet[*]}
+
+headPercentage=$(($headCount*100/5))
+echo "headPercentage is :" $headPercentage
+
+tailPercentage=$(($tailCount*100/5))
+echo "tailPercemtage is :" $tailPercentage
